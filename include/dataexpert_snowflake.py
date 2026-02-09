0a80edd8c3ea918aec222e716761a902e05124b4
Miguel Almeida <miguelmoutela@hotmail.com>
2026-02-09 20:47:12 +0000

    ported sec api for report - docs - extraction

--- include/dataexpert_snowflake.py
+++ include/dataexpert_snowflake.py
@@ -4,20 +4,30 @@ from contextlib import contextmanager
 from cryptography.hazmat.primitives import serialization
 from snowflake.snowpark import Session
 
+key_path = os.path.expanduser(os.environ.get("SF_PRIVATE_KEY_PATH"))
+passphrase = os.environ.get("SF_PRIVATE_KEY_PASSPHRASE")
+password_bytes = passphrase.encode() if passphrase else None
+
+
+with open(key_path, "rb") as f:
+    p_key = serialization.load_pem_private_key(
+        f.read(),
+        password=password_bytes
+    )
+
+session = Session.builder.configs({
+    "account": os.environ["SF_ACCOUNT"],
+    "user": os.environ["SF_USER"],
+    "private_key": p_key,
+    "role": os.environ["SF_ROLE"],
+    "warehouse": os.environ["SF_WAREHOUSE"],
+    "database": os.environ["SF_DATABASE"],
+    "schema": os.environ["SF_SCHEMA"],
+}).create()
+
+
 @contextmanager
 def connect():
-    print("snow cx")
-
-    key_path = os.path.expanduser(os.environ.get("SF_PRIVATE_KEY_PATH"))
-    passphrase = os.environ.get("SF_PRIVATE_KEY_PASSPHRASE")
-    password_bytes = passphrase.encode() if passphrase else None
-    
-  
-    with open(key_path, "rb") as f:
-        p_key = serialization.load_pem_private_key(
-            f.read(),
-            password=password_bytes
-        )
 
     # private_key_bytes = p_key.private_bytes(
     #     encoding=serialization.Encoding.DER,
@@ -30,17 +40,11 @@ def connect():
     #     password=os.environ["SF_PRIVATE_KEY_PASSPHRASE"].encode(),
     # )
 
-    session = Session.builder.configs({
-        "account": os.environ["SF_ACCOUNT"],
-        "user": os.environ["SF_USER"],
-        "private_key": p_key,
-        "role": os.environ["SF_ROLE"],
-        "warehouse": os.environ["SF_WAREHOUSE"],
-        "database": os.environ["SF_DATABASE"],
-        "schema": os.environ["SF_SCHEMA"],
-    }).create()
-
     try:
         yield session
     finally:
         session.close()
+
+
+def get_snowpark_session(schema: str):
+    return session 
